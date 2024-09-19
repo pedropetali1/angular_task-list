eea2745b783de1cfb63f2d48c964eb636a7cdff7
Pedro Lucas <pedrolucastpetali@hotmail.com>
2024-09-19 10:10:09 -0300

    first commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {UiService} from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
+  showAddTask: boolean = true;
+  subscription!: Subscription
+  
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value))
+   }
+
+  ngOnInit(): void {
+  }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+}
---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Task} from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+  @Input() task!: Task
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+
+}
